d3f95155d250c85cfa2dd8a8db5203377bbfeb57
John Higgins M. Avila <johnhiggins.avila@gmail.com>
2018-10-27 22:28:53 +0800

    Initial Commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const rp = require('request-promise');
+const app = express();
+
+// Cors
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
+
+const URL = 'https://35.240.172.28/middleware-api';
+
+app.get('/echo', function (req, res, next) {
+  rp.get({
+    uri: URL + '/echo',
+    headers: {
+      'content-type': 'application/json'
+    }
+  })
+    .then(function (response) {
+      res.status(200).send(response);
+    })
+    .catch(function (err) {
+      res.status(500).send(err);
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log('App listening on port ' + PORT + '!');
+});
